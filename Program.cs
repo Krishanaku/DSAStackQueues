4dc994966e6444c19fcdcece08254588ee1729c1
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-24 18:05:30 +0530

    Add : Ability to create a Queue of 56->30->70 - Use LinkedList to do the Queue Operations

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAStackQueues
+{
+    public class Program
+    { 
+        public static void Main(string[] args) 
+        {
+            LinkedList list = new LinkedList();
+            Queue q1 = new Queue();
+            q1.Enqueue(56);
+            q1.Enqueue(30);
+            q1.Enqueue(70);
+
+            foreach (int i in q1)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine("\nTop of the stack : {0}", q1.Peek());
+            Console.WriteLine("************************");
+        }
+    }
+}
